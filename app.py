e44f4546ce680e1289e39b294e4642bcd08559fb
chenxing <494533577@qq.com>
2018-11-12 18:59:06 +0800

    new

---
+++ app.py
@@ -0,0 +1,239 @@
+from flask import Flask,render_template,url_for,request,redirect,Blueprint,session,flash
+from pymongo import MongoClient
+import datetime,os,re,json,random
+from fuzzywuzzy import fuzz,process
+from flask_login import LoginManager,UserMixin,login_required,current_user,login_user,logout_user
+
+app = Flask(__name__)
+conn = MongoClient('localhost', 27017)
+db = conn.youtube_db
+#user-------------
+app.secret_key = 'ds8cix1'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.login_message_category = "info"
+login_manager.login_message = u"请登录！"
+login_manager.init_app(app=app)
+auth = Blueprint('auth',__name__)
+class User(UserMixin):
+    pass
+
+def query_user(username):
+    users = []
+    for each in db.user.find({},{'_id':0}):
+        users.append(each)
+    for user in users:
+        if user['username'] == username:
+            return user
+
+# 如果用户名存在则构建一个新的用户类对象，并使用用户名作为ID
+# 如果不存在，必须返回None
+@login_manager.user_loader
+def load_user(username):
+    if query_user(username) is not None:
+        curr_user = User()
+        curr_user.id = username
+        return curr_user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        user = query_user(username)
+        # 验证表单中提交的用户名和密码
+        if user is not None and request.form['password'] == user['password']:
+            curr_user = User()
+            curr_user.id = username
+            # 通过Flask-Login的login_user方法登录用户
+            login_user(curr_user,remember=True)
+            # 如果请求中有next参数，则重定向到其指定的地址，
+            # 没有next参数，则重定向到"index"视图
+            next = request.args.get('next')
+            return redirect(next or url_for('index'))
+        flash('用户名或密码错误 !')
+    # GET 请求
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        if request.form:
+            user_dic = {}
+            user_dic['username'] = request.form.to_dict()['username']
+            user_dic['password'] = request.form.to_dict()['password']
+            user_dic['if_member'] = False
+            user_dic['member_end_date'] = None
+            user_dic['email'] = None
+            user_dic['phone'] = request.form.to_dict()['phone']
+            user_dic['upload_file'] = None
+            user_dic['user_tag'] = None
+            user_dic['review_history'] = None
+            db.user.insert(user_dic)
+        return redirect(url_for('index'))
+    # GET 请求
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/search',methods=['GET','POST'])
+def search():
+    dic = {}
+    if request.method == 'POST':
+        if request.form.to_dict():
+            keyword = request.form.to_dict()['search']
+            choices = []
+            main_info = []
+            temp = []
+            for each in db.col.find():
+                choices.append(each['tags'])
+            relate = process.extract(keyword, choices, limit=60, scorer=fuzz.partial_token_set_ratio)
+            [temp.append(i) for i in relate if not i in temp]
+            for each in temp:
+                main_info.append(db.col.find_one({'tags': each[0]}, {'_id': 0}))
+
+            dic['main_info'] = main_info
+            dic = json.dumps(dic)
+    return render_template('search.html',dic = dic)
+
+
+@app.route('/watch',methods=['GET','POST'])
+def video():
+    username = current_user.get_id()
+    user_info = {'login':'false'}
+    if username is not None:
+        user_info = query_user(username)
+        user_info.pop('password')
+        user_info['login'] = 'true'
+    id = request.args.get('id')
+    main_info = db.col.find_one({'video_id':id},{'_id': 0 })
+    playlist_info = []
+    temp_list = []
+    for each in main_info['playlist']:
+        temp = db.col.find_one({'video_id': each},{'_id': 0 })
+        if temp:
+            playlist_info.append(temp)
+    relate = db.col.find({'categories':main_info['categories']},{'_id': 0})
+    for each in relate:
+        temp_list.append(each)
+    if len(temp_list)>10:
+        relate_info = random.sample(temp_list, 10)
+    else:
+        relate_info = temp_list
+    vi_dic = {}
+    vi_dic['main_info'] = main_info
+    vi_dic['playlist_info'] = playlist_info
+    vi_dic['relate_info'] = relate_info
+    user_info = json.dumps(user_info)
+    return render_template('video_main.html',vi_dic = vi_dic,user_info =user_info)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    dic = {}
+    main_list = []
+    temp_list = []
+    temp_categories = [['People & Blogs'], ['Science & Technology'], ['Education'], ['Music'], ['Entertainment'], ['Comedy'], ['Howto & Style'],['Film & Animation'], ['Sports'], ['Autos & Vehicles'], ['Gaming'], ['News & Politics'], ['Pets & Animals'], ['Nonprofits & Activism'], ['Shows'], ['Travel & Events']]
+    temp_categories = random.sample(temp_categories,len(temp_categories))
+    # temp_categories = []
+    # for each in db.col.find():
+    #     if each['categories'] not in temp_categories:
+    #         temp_categories.append(each['categories'])
+    # print(temp_categories)
+    for item in temp_categories:
+        for each in db.col.find({'categories': item}, {'_id': 0,'categories':1,'video_id':1,'title':1,'duration':1,'view_count':1,'upload_date':1,'creator':1,'thumbnail':1}):
+            temp_list.append(each)
+        if len(temp_list) > 10:
+            temp_list = random.sample(temp_list, len(temp_list))
+            main_list.append(temp_list)
+        # else:
+        #     main_list.append(temp_list)
+        temp_list = []
+    dic['main_list'] = main_list
+    dic = json.dumps(dic)
+    return render_template('index.html',title='欢迎来到MixWheel',dic = dic)
+
+@app.route('/delete',methods=['GET','POST'])
+def delete():
+    id = request.args.get('id')
+    try:
+        os.remove('./myweb/static/images/%s.gif'%id)
+    except:
+        pass
+    try:
+        os.remove('./myweb/static/images/%s.jpg'%id)
+    except:
+        pass
+    try:
+        os.remove('./myweb/static/video/%s.mp4'%id)
+    except:
+        pass
+    db.col.remove({'video_id': id})
+    return id+' deleted'
+
+@app.route('/admin',methods=['GET','POST'])
+def admin():
+    main_info = []
+    dic = {}
+    randint = random.randint(0, 10)
+    for each in db.col.find({'random': {'$gte': randint, '$lte': randint+90}},{'_id': 0}):
+        main_info.append(each)
+    dic['main_info'] = main_info
+    dic = json.dumps(dic)
+    return render_template('admin_video.html', dic=dic)
+
+@app.route('/register',methods=['GET','POST'])
+def signup():
+    return render_template('register.html')
+
+@app.route('/userValidate',methods=['GET','POST'])
+def userValidate():
+    username = request.args.get('username')
+    if db.user.find({'username':username}).count():
+        return '该用户名已被注册!'
+    else:
+        return ''
+
+@app.route('/test',methods=['GET','POST'])
+def test():
+    return render_template('test.html')
+
+def user_mail_confir(address):
+    # 导入python里面的这个stmplib这个库
+    import smtplib
+    # 导入邮件文本模块
+    from email.mime.text import MIMEText
+
+    # 设置SMTP服务器
+    SMTPServer = "smtp.qq.com"
+    # 设置发邮件的地址,也就是自己的邮箱地址.
+    sender = "nj17****8@163.com"
+    # 邮箱的密码,注意这是你自己的密码哦
+    passwd = "lalalala**"
+
+    # 设置发送的内容
+    message = "今晚上山打老虎!"
+    # 转换为邮件文本,也就是用我们导入的模块email.mime.text的MIMEText方法进行转换
+    msg = MIMEText(message)
+    # 设置邮件标题
+    msg["Subject"] = "来自帅哥的问候"
+    # 设置发送者的名称
+    msg["From"] = sender
+    # 设置好一切基本条件以后,万事俱备了,开始进行连接了.
+    # 创建STMP服务器 ,连接STMP的服务器
+    mailServer = smtplib.SMTP(SMTPServer, 25)  # 25是邮件专用的端口哦.
+    # 登录邮箱
+    mailServer.login(sender, passwd)
+    # 发送邮件
+    mailServer.sendmail(sender, ["957**8@qq.com", "4449***454@qq.com", "nj17449***8@163.com"], msg.as_string())
+    # 发送完毕以后,记得退出,怎么样,是不是很简单呢?
+    mailServer.quit()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ youtubeSpider.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+import requests,os,re,time,json,shutil,random,threading
+from bs4 import BeautifulSoup
+from pytube import YouTube,Playlist
+import youtube_dl
+from pymongo import MongoClient
+from moviepy.editor import *
+
+class youtube_link():
+    def __init__(self):
+        self.conn = MongoClient('localhost', 27017)
+        self.db = self.conn.youtube_db
+        self.play_type = {'list_mark':'yt-lockup yt-lockup-tile yt-lockup-playlist vve-check clearfix','video_mark':'yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix'}
+        self.srt_key = 'a2d09c7d76fced01f8be4b1f4cce8bec'
+    def youtube_info(self,id):
+        link = 'https://www.youtube.com/watch?v=' + id
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+        with ydl:
+            try:
+                result = ydl.extract_info(link, download=False)
+                if 'entries' in result:
+                    # Can be a playlist or a list of videos
+                    video = result['entries'][0]
+                else:
+                    # Just a video
+                    video = result
+            except:
+                video = None
+                if not self.check_link(id):
+                    self.db.black_col.insert({'black_id':id})
+                print('extract_info eroor')
+
+        return video
+
+    def srt_content(self,id):
+        srt_dic = {'en-auto':'','zh-Hans':''}
+        link = []
+        dic = json.loads(requests.get('https://api.zhuwei.me/v1/captions/' + id + '?api-key=' + self.srt_key).text)
+        if dic['meta']['msg'] == 'OK':
+            captions = dic['response']['captions']['available_captions']
+            if captions:
+                for each in captions:
+                    if re.search('en',each['language']) or re.search('zh-Hans',each['language']):
+                        link.append(each['caption_content_url'])
+            if link:
+                for each in link:
+                    re_dic = json.loads(requests.get(each + '?api-key=' + self.srt_key).text)
+                    if re_dic:
+                        try:
+                            print('srt limit '+ str(re_dic['meta']['limit_remaining']))
+                        except:
+                            pass
+                        if re_dic['meta']['msg'] == 'OK':
+                            srt_dic[str(each.split('/')[-1])] = re_dic['contents']['content']
+                        else:
+                            srt_dic[str(each.split('/')[-1])] = ''
+        return srt_dic
+
+    def get_list(self,link):
+        playlist_id = []
+        if re.search('list=',link):
+            res = requests.get(link)
+            soup = BeautifulSoup(res.text)
+            item = soup.find('div', class_='playlist-videos-container yt-scrollbar-dark yt-scrollbar').find_all('a')
+            for each in item:
+                playlist_id.append(each.get('href')[9:20])
+        return playlist_id
+
+    def prase(self,playlist_id):#检查playlist_id下载的完整度
+        for id in playlist_id:
+            if not self.check_link(id):
+                json_dic = self.youtube_info(id)
+                if json_dic:
+                    self.db_insert(json_dic, playlist_id)
+
+    def sec_to_hour(self,sec):
+        m, s = divmod(sec, 60)
+        h, m = divmod(m, 60)
+        return("%d:%02d:%02d" % (h, m, s))
+
+    def db_insert(self,json_dic,playlist_id):
+        insert_dic = {}
+        insert_dic['video_id'] = json_dic['id']
+        insert_dic['thumbnail'] = json_dic['thumbnail']
+        insert_dic['tags'] = json_dic['tags']
+        insert_dic['tags'].append(self.keyword)
+        insert_dic['categories'] = json_dic['categories']
+        insert_dic['keyword'] = self.keyword
+        insert_dic['upload_date'] = json_dic['upload_date']
+        insert_dic['creator'] = json_dic['creator']
+        insert_dic['uploader'] = json_dic['uploader']
+        insert_dic['title'] = json_dic['title']
+        temp_dic = {}
+        temp_dic['sec'] = json_dic['duration']
+        temp_dic['hour'] = self.sec_to_hour(int(json_dic['duration']))
+        insert_dic['duration'] = temp_dic
+        insert_dic['view_count'] = json_dic['view_count']
+        insert_dic['like_count'] = json_dic['like_count']
+        insert_dic['dislike_count'] = json_dic['dislike_count']
+        insert_dic['description'] = json_dic['description']
+        temp_dic = {}
+        try:
+            temp_srt = self.srt_content(json_dic['id'])
+            for lan in temp_srt:
+                f = open('./static/srt/' + json_dic['id'] + '_' + lan + '.srt', 'w', encoding='utf-8')
+                f.write(temp_srt[lan])
+                f.close()
+                temp_dic[lan] = 'srt/' + json_dic['id'] + '_' + lan + '.srt'
+        except:
+            temp_dic['zh-Hans'] = ''
+        insert_dic['subtitle'] = temp_dic
+        insert_dic['video_url'] = 'https://www.youtube.com/watch?v=' + json_dic['id']
+        insert_dic['playlist'] = playlist_id
+        insert_dic['random'] = random.randint(0,100)
+        self.db.col.insert(insert_dic)
+        print('---->insert '+ json_dic['id'] +' success<----')
+    def search(self,keyword,list = False):
+        self.keyword = keyword
+        if list :
+            res = requests.get('https://www.youtube.com/results?search_query='+ self.keyword)
+        else:
+            res = requests.get('https://www.youtube.com/results?search_query=' + self.keyword +'&sp=EgIQAw%253D%253D')
+        soup = BeautifulSoup(res.text)
+        one_video = soup.find_all('div', class_=self.play_type['video_mark'])
+        video_list = soup.find_all('div', class_=self.play_type['list_mark'])
+        for each in one_video:
+            playlist_id = []
+            playlist_id.append(each.find('a').get('href')[9:20])
+            self.prase(playlist_id)
+        for each in video_list:
+            playlist_id = self.get_list('https://www.youtube.com'+ each.find('a').get('href'))
+            self.prase(playlist_id)
+
+    def load_list(self,keyword,link):
+        self.keyword = keyword
+        self.prase(self.get_list(link))
+
+    def check_link(self,id):
+        if self.db.black_col.find({'video_id': id}).count() or self.db.col.find({'video_id':id}).count():
+            return True
+
+    def down_load(self,playlist = False,img = False,video = False,keyword = False):
+        video_down_list = []
+        if playlist:
+            self.update_playlist()
+        for item in self.db.col.find():
+            if keyword:
+                if not item['keyword']:
+                    self.db.col.update({'_id': item['_id']},{'$set': {'keyword': item['tags'][0]}})
+            if video:
+                if re.search('www.youtube.com',item['video_url']):
+                    temp_list = [item['video_url'],item['video_id']]
+                    video_down_list.append(temp_list)
+                    temp_list =[]
+
+            if img:
+                if re.search('https',item['thumbnail']):
+                    print('Downloading ' + item['video_id'] + '.....')
+                    f = open('./static/images/%s.jpg'%item['video_id'] , 'ab')
+                    f.write(requests.get(item['thumbnail']).content)
+                    f.close()
+                    self.db.col.update({'_id': item['_id']}, {'$set': {'thumbnail': 'images/%s.jpg'%item['video_id']}})
+                    print('download ' + item['video_id'] + '.jpg success')
+        for x in video_down_list[:10]:
+            t = threading.Thread(target=self.thread_video, args=(x[0],x[1]))
+            t.start()
+
+
+    def thread_video(self,video_url,video_id):
+        try:
+            print('Downloading ' + video_url + '.....')
+            yd_ = YouTube(video_url)
+            yd_.streams.filter(progressive=True, subtype='mp4').first().download('./static/video/', video_id)
+            print('download ' + video_url + ' success')
+            self.db.col.update({'video_id': video_id}, {'$set': {'video_url':'http://my-mixwheel.oss-cn-zhangjiakou.aliyuncs.com/myweb/static/video/%s.mp4'%video_id}})
+            self.db.col.update({'video_id': video_id}, {'$set': {'test_url': 'video/%s.mp4' % video_id}})
+        except:
+            print('Downloading ' + video_url + ' failed')
+            # self.db.col.remove({'video_id': video_id})
+
+    def update_playlist(self):
+        for item in self.db.col.find():
+            self.prase(item['playlist'])
+            self.keyword = None
+
+    def creat_gif(self):
+        i=0
+        for item in self.db.col.find():
+            if not re.search('www.youtube.com', item['test_url']):
+                if not os.path.exists("./static/images/%s.gif" % item['video_id']):
+                    print(item['video_id'])
+                    try:
+                        clip = (VideoFileClip('./static/%s'%item['test_url']).subclip(80,83).resize((240,135)))
+                        clip.write_gif("./static/images/%s.gif"%item['video_id'])
+                    except :
+                        print(i)
+                        if i>5:
+                            exit()
+                        i+=1
+                        # print(Exception)
+                        shutil.copyfile("./static/images/%s.jpg"%item['video_id'], "./static/images/%s.gif"%item['video_id'])
+
+yt = youtube_link()
+# threads = []
+# def fun01():
+#     yt.search('Pets & Animals')
+# def fun02():
+#     yt.load_list('音乐 music',link='https://www.youtube.com/watch?v=o5pSCyzUpqM&list=PLvfcJFF9Y5Qj8NGBoQGGniN0TT-mgVaKl')
+# def fun03():
+#     yt.down_load(video=True)
+# threads.append(threading.Thread(target=fun03))
+# threads.append(threading.Thread(target=fun02))
+# threads.append(threading.Thread(target=fun03))
+
+# print(yt.youtube_info('Wr7nlnRq3tU'))
+# yt.search('吉米今夜秀')
+
+# yt.load_list('极限运动',link='https://www.youtube.com/watch?v=Gpl_kkCXzPg&list=PLjA5LSvsPoRTBLGsXeO9qSzrrW2Z2EX86')
+yt.down_load(video = True)
+
+# yt.creat_gif()
+
+# pl = ['0E1sDpiVQv0']
+# yt.keyword='电影'
+# yt.prase(pl)
+
+
+
